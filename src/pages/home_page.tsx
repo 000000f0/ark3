b28ea49f78282abfdd4cdaf101a55f4d845ac0c9
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-04-21 22:10:47 +0200

    update

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
 
 type HomePageProps = {
   isDark: boolean;
@@ -7,12 +9,26 @@ type HomePageProps = {
 
 const HomePage: React.FC<HomePageProps> = ({ isDark }) => {
   return (
-    <main style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>
+    <div>
+
+
+
+
+    <div className='home1' style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>
       <div>
         <h1>The Energy Company<br />You Own</h1>
         <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' }}>About</button></Link>
       </div>
-    </main>
+    </div>
+    
+<div className='home2' style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>
+      <div>
+        <h1>Meet Up<br />AMA</h1>
+        <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' }}><FontAwesomeIcon icon={faBroadcastTower} /></button></Link>
+      </div>
+    </div>
+
+    </div>
   );
 };
 
