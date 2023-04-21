fe29e4b9a8347af9a7358a5371fa13693225dbb5
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-04-21 23:02:11 +0200

    update

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -16,15 +16,19 @@ const HomePage: React.FC<HomePageProps> = ({ isDark }) => {
 
     <div className='home1' style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>
       <div>
-        <h1>The Energy Company<br />You Own</h1>
-        <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' }}>About</button></Link>
+      <h1 style={{ textShadow:  !isDark ? '0 0 1px #000, 0 0 2px #000, 0 0 3px #000, 0 0 4px #000'
+      :'0 0 1px #fff, 0 0 2px #fff, 0 0 3px #fff, 0 0 4px #fff'
+       }}>The Energy Company<br />You Own</h1>
+        <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' , border: isDark ? '1px solid white' : '1px solid black'}}>About</button></Link>
       </div>
     </div>
     
 <div className='home2' style={{ backgroundColor: isDark ? '#fff' : '#000', color: isDark ? '#000' : '#fff' }}>
       <div>
-        <h1>Meet Up<br />AMA</h1>
-        <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' }}><FontAwesomeIcon icon={faBroadcastTower} /></button></Link>
+        <h1 style={{ textShadow:  !isDark ? '0 0 1px #000, 0 0 2px #000, 0 0 3px #000, 0 0 4px #000'
+      :'0 0 1px #fff, 0 0 2px #fff, 0 0 3px #fff, 0 0 4px #fff'
+       }}>Meet Up<br />Announcements</h1>
+        <Link to="/about"><button style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000' , border: isDark ? '1px solid white' : '1px solid black'}}><FontAwesomeIcon icon={faBroadcastTower} /></button></Link>
       </div>
     </div>
 
