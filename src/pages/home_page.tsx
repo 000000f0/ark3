e14b4225b3451f89bc66e43273e504dcf96eb5bc
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-04-21 15:56:59 +0200

    update

--- src/pages/home_page.tsx
+++ src/pages/home_page.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage: React.FC = () => {
   return (
     <main>
-    <div>
-      <h1>the energy company<br />
-you own</h1>
-      
-
-
-
-    </div>
+      <div>
+        <h1>The Energy Company<br />You Own</h1>
+        <Link to="/about"><button >About</button></Link>
+      </div>
     </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   );
 };
 
 export default HomePage;
+
+
+
+
+
+
+
+
+
