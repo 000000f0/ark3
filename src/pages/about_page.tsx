40d49c6da4a1f7a9585693c9c67445f6b652d9d7
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-04-21 17:07:28 +0200

    update

--- src/pages/about_page.tsx
+++ src/pages/about_page.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 const AboutPage: React.FC = () => {
   return (
     
-    <div className='main4'>
+    
+      <div className='grey_background'>
+        <center>
       <div className="banner">
-  <iframe width="100%" height="500" src="https://www.youtube.com/embed/VIDEO_ID" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+        
+  <iframe width="60%" height="500" src="https://www.youtube.com/embed/VIDEO_ID" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+  
 </div>
+</center>
+<div className='main4'>
       <h1>Welcomasdfamy website!</h1>
       <p>Thank you for visiting. Please take a look around and let me know if you have any questions.</p>
 
@@ -28,16 +34,30 @@ const AboutPage: React.FC = () => {
       <p >There are problems in the world which communities are solving in their own way.focus of Ark4 is building a sustainable future for our future generations through self-sustaining descentralized economies.<br />‍<br />You may ask what about the planet, are we doing our bit? Are we a green company? Yes. <br />Is it our main focus? No.<br /><br />We are a green company in that we utilize renewable resources and our output is sustainable.<br /><br />Ark4 originates in Southern Africa, where the ever increasing power shortage is br /inging economies to their knees. The escalating cost of living and worries over energy sources, though, is a global problem!<br /><br />So in a world so reliant on energy, how do we take back our voice, our control?<br />You own the power! It is the key to economic freedom!<br />This is where the blockchain comes in and where the Dao comes into play. Everyone has a voice, a vote, a meaningful say with full transparency.<br />‍<br />To be bold, this is how we change the world. We can’t do it alone. We start, we build and we collaborate with like minded organisations. Organisations aligned to the voice of their communities.<br /><br />Some might view this as a tall objective, perhaps a mountain. Even Everest was conquered, but not in a day. <br />We start small. We start one volt at a time.<br /><br />We grow. We do it together.<br /><br />We are taking action. <br />Are you?<br /></p>
     </div>
   </div>
-
-
-
+  </div>
+  <div className='founders'>
+  <h2>Meet the Founders</h2>
+  <div className='founders-container'>
+    <div className='founders-img'>
+      <img src='https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/founder_1.jpeg' alt='Founder 1' />
+      <h3>Sean</h3>
+      <p>Co-Founder & CEO</p>
     </div>
+    <div className='founders-img'>
+      <img src='https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/founder_2.jpeg' alt='Founder 2' />
+      <h3>Sean</h3>
+      <p>Co-Founder & CTO</p>
+    </div>
+  </div>
+</div>
+
+    
     
 
 
 
 
-
+</div>
 
 
 
