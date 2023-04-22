e108b9ba0976fba90b48c04e0a183a5877fdc510
Willie deJongh <williedejongh@Willies-MacBook-Pro-2.local>
2023-04-22 15:03:28 +0200

    update

--- src/pages/about_page.tsx
+++ src/pages/about_page.tsx
@@ -17,21 +17,40 @@ const AboutPage: React.FC<AboutPageProps> = ({ isDark }) => {
   
 </div>
 </center>
+
+
 <div className='main4'  style={{ backgroundImage: isDark ? 'radial-gradient( ellipse farthest-corner at 12px 12px , #0c0c0c, #0c0c0c 50%, #000000 50%)' : 'radial-gradient( ellipse farthest-corner at 12px 12px , #ebebeb, #ebebeb 50%, #ffffff 50%)', color: isDark ? 'white':'black'}}>
      
 
      
-     <h1>Welcomasdfamy website!</h1>
-      <p>Thank you for visiting. Please take a look around and let me know if you have any questions.</p>
+     <h1>Ark4</h1>
       
       
 
-      <h1 ></h1>
-      <p >There are problems in the world which communities are solving in their own way.
-        
-        The focus of Ark4 is building a sustainable future for our future generations through self-sustaining descentralized economies.
-        
-        You may ask what about the planet, are we doing our bit? Are we a green
+      <h3 >Why?</h3>
+      <p> There are problems in the world which communities are solving in their own way.
+
+The focus of Ark4 is building a sustainable future for our future generations through self-sustaining descentralized economies.
+‍
+You may ask what about the planet, are we doing our bit? Are we a green company? Yes.
+Is it our main focus? No.
+
+We are a green company in that we utilize renewable resources and our output is sustainable.
+
+Ark4 originates in Southern Africa, where the ever increasing power shortage is bringing economies to their knees. The escalating cost of living and worries over energy sources, though, is a global problem!
+
+So in a world so reliant on energy, how do we take back our voice, our control?
+You own the power! It is the key to economic freedom!
+This is where the blockchain comes in and where the Dao comes into play. Everyone has a voice, a vote, a meaningful say with full transparency.
+‍
+To be bold, this is how we change the world. We can’t do it alone. We start, we build and we collaborate with like minded organisations. Organisations aligned to the voice of their communities.
+
+Some might view this as a tall objective, perhaps a mountain. Even Everest was conquered, but not in a day.
+We start small. We start one volt at a time.
+
+We grow. We do it together.
+
+We are taking action
         
 </p>
 
@@ -44,6 +63,80 @@ const AboutPage: React.FC<AboutPageProps> = ({ isDark }) => {
     </div>
   </div>
   </div>
+
+  <div className='main4'  style={{ backgroundImage: isDark ? `url('https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/vision_render1.jpg')` : `url('https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/vision_render1.jpg')`, background: 'no-repeat', backgroundSize: "cover",
+  backgroundPosition: "center",backgroundColor: isDark ? '#000' : '#fff', color: isDark ? 'white':'black' }}>
+<h2 >Beta Close</h2>
+      <p> Beta Close is a new gen farm for a new generation.
+‍
+Join us in Turning The Sun into Dollars for You, Now!
+
+A physical (IRL) sustainable farm within a self-governing, decentralized economies.
+We envisage this to contain many such farms globally.
+
+Ark4 is continuously watching and experimenting with the power of Solar, Wind, Water and Earth to develop future sustainable energy sources.
+
+Our focus is on building core infrastructure for sustainable economies in the real world and digital platforms such as Web3 and the Metaverse.
+‍
+We achieve this through various digital communication platforms that enable you to have a voice.
+</p>
+</div>
+
+  <div className='main4'  style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? 'white':'black'}}>
+     
+
+     
+      
+
+      <h2 >Power</h2>
+      <p> Ark4 introduces the monetization of energy, providing a revenue stream and building the core infrastructures.
+
+We need energy to become self-sustainable. Energy powers our world and we cannot thrive without it.
+        
+</p>
+
+
+
+</div>
+
+<div className='main4'  style={{ backgroundColor: isDark ? '#013220' : '#90EE90', color: isDark ? 'white':'black'}}>
+     
+
+     
+      
+
+      <h2 >Green Mining</h2>
+      <p> Sustainable BTC Mining is the key to our monetization strategy.
+‍
+We achieve this through a combination of our mining equipment and partnerships with global players.
+
+It is a well known fact that the largest cost of mining is power.
+Our BTC mining is powered by 100% renewable energy, providing sustainable returns for you.
+We are mining green. BTC green.
+</p>
+
+
+
+</div>
+<div className='main4'  style={{ backgroundColor: isDark ? '#000' : '#fff',
+  color: isDark ? 'white' : 'black',}}>
+
+      
+
+      <h2 ><img width="30px" style={{marginBottom:'-6px'}} src={isDark? 'https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/dao_dark.jpg':'https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/dao_light.jpg'}/> DAO</h2>
+      <p> We utilise the blockchain as the foundation of Ark4’s governance.
+The DAO gives the community a voice and enables each of us to be heard.
+
+Click Here to secure your own Dollar based NFT and become part of a sustainable energy future where your voice counts.
+
+Membership includes:
+ Ownership without the fear of having it taken away from you.
+ Income through investment and yields.
+ Empowerment through freedom and returning fundamental rights to the people.
+ A future through skills, training and upliftment.
+</p>
+
+</div>
   <div className='founders'>
   <h2>Meet the Founders</h2>
   <div className='founders-container'>
--- src/pages/mint_page.tsx
+++ src/pages/mint_page.tsx
@@ -13,11 +13,11 @@ const MintPage: React.FC<MintPageProps> = ({ isDark }) => {
     <table className='nft-table' style={{ backgroundColor: isDark ? '#000' : '#fff', border: isDark ? '1px solid white':'1px solid black', boxShadow: !isDark ?  '0 0 10px rgba(0, 0, 0, 0.5)':'0 0 10px rgba(255, 255, 255, 0.5)'}}>
       <tbody>
         <tr>
-          <td  style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000'}}>Pugly, the pug</td>
+          <td  style={{ backgroundColor: isDark ? '#000' : '#fff', color: isDark ? '#fff' : '#000'}}><h1>Pugly, the pug</h1></td>
         </tr>
         <tr>
           <td className='nft-image-cell'>
-            <img style={{ height: '300px' }} className='nft-image' src='https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/pugly.jpeg' />
+            <img  className='nft-image' src='https://amplify-amplifya785c969872c4-staging-111600-deployment.s3.amazonaws.com/pugly.jpeg' style={{ height: '300px',  backgroundColor: isDark ? '#000' : '#fff', border: isDark ? '1px solid white':'1px solid black', boxShadow: !isDark ?  '0 0 10px rgba(0, 0, 0, 0.5)':'0 0 10px rgba(255, 255, 255, 0.5)'}} />
           </td>
         </tr>
         <tr>
